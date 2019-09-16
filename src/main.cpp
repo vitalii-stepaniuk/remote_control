40141e1a2a9461b0108db4dbcb90d7b33d219cbf
Vitalii Stepaniuk <vitalii.stepaniuk@gmail.com>
2019-09-16 22:31:26 +0300

    Wifi connection.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <Arduino.h>
+#include <WiFiClient.h>
+#include <ESP8266WiFi.h>
+#include <config.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  WiFi.begin(SSID, PASSWORD);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(SSID);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
 }
 
 void loop() {
